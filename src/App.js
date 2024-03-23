c762dd0fd9aec65a2aa162066f8e7b7a67a763a5
bryanongjx <bryanong1589@gmail.com>
2024-03-23 19:53:55 +0800

    change gif

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <div>u got disss!</div>
         <br/>
-        <AnimatedGif src = "https://i.pinimg.com/originals/1d/68/a1/1d68a1a386fc02e90bfe4e5d37e9e67d.gif" alt = 'oops gif failed to load'/>
+        <AnimatedGif src = "https://i.pinimg.com/originals/40/4a/3e/404a3e2430b4739a3132f651d564af2e.gif" alt = 'oops gif failed to load'/>
       </header>
     </div>
   );
